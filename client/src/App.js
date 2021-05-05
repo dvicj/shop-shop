7d778bbbe3b79a74097b42c9e648e7c40498373a
dvicjon <devinvjones@gmail.com>
2021-05-05 18:26:03 -0400

    22.4.6

--- client/src/App.js
+++ client/src/App.js
@@ -8,9 +8,11 @@ import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import Success from "./pages/Success";
 import Nav from "./components/Nav";
 import { StoreProvider } from "./utils/GlobalState";
 import OrderHistory from "./pages/OrderHistory";
+
 
 const client = new ApolloClient({
   request: (operation) => {
@@ -37,6 +39,7 @@ function App() {
             <Route exact path="/signup" component={Signup} />
             <Route exact path="/orderHistory" component={OrderHistory} />
             <Route exact path="/products/:id" component={Detail} />
+            <Route exact path="/success" component={Success} />
             <Route component={NoMatch} />
           </Switch>
           </StoreProvider>
